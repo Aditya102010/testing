70e0540527ebb9092ac7ebece0dda9821fb58234
Aditya <aditya-pratap.singh@hpe.com>
2023-03-29 19:39:34 +0530

    Added docstrings

--- app.py
+++ app.py
@@ -1,101 +1,104 @@
-from flask import Flask, redirect, url_for, request,render_template,send_file
+from flask import Flask, redirect, url_for, request,render_template,send_file, session
 from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate, migrate
- 
-
+import datetime
 import sqlite3
 import base64
-import binascii,os
+import binascii
+import os
 import bcrypt
 
-
-
+#creating an instance of Flask app
 app = Flask(__name__,template_folder='templates')
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Ciphers.db'
 app.config['SQLALCHEMY_BINDS'] = { 'user': 'sqlite:///User.db',
-'role': 'sqlite:///Role.db'
+'role': 'sqlite:///Role.db',
+'log' : 'sqlite:///Logs.db'
 
 }
     
 db = SQLAlchemy(app)
 
-# Settings for migrations
-#migrate = Migrate(app, db)
-
 app.app_context().push()
 
 
 class User(db.Model):
-    # Model for saving new user details
-    # This shall be saved into database in encrypted format too.
+
+    """ Model for saving new user details
+    The password gets saved in encrypted format (using bcrypt)."""
+    __bind_key__ = 'user'
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.VARCHAR(50),nullable = False)
     username = db.Column(db.VARCHAR(50), nullable = False)
     password = db.Column(db.VARCHAR(50), nullable = False)
     
- 
-    # repr method represents how one object of this datatable
-    # will look like
-    def __repr__(self):
-        return f"New USER created with name "
+
 class Role(db.Model):
-    # creating the role for each new user added
+    __bind_key__  = 'role'
+    """ creating the role for each new user added """
     id = db.Column(db.Integer,primary_key= True)
     username = db.Column(db.VARCHAR(50), nullable = False)
     role = db.Column(db.VARCHAR(50), nullable = False)
 
-    def __repr__(self):
-        return f"New User Role created"
 
 class ciphers(db.Model):
-    # saving the encrypted texts
+
+    """Saving all the ciphertexts """
     id = db.Column(db.Integer,primary_key = True)
     cipher = db.Column(db.VARCHAR(50),nullable = False)
-    
- 
-    # repr method represents how one object of this datatable
-    # will look like
-    def __repr__(self):
-        return f"New entry created "
+    date = db.Column(db.VARCHAR(50), nullable = False)
+
+class Logs(db.Model):
+    __bind_key__ = 'log'
+    "Logging each entry made by any certain user on the server side"
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.VARCHAR(50), nullable = False)
+    cipher = db.Column(db.VARCHAR(50), nullable = False)
+    date = db.Column(db.VARCHAR(50), nullable = False) 
 
 
-@app.route('/')  #decorator for route(argument) function
-def first_page():# renders the first page   
+@app.route('/')
+def first_page():
+
+    """ Renders the homepage """
     return render_template('form.html') 
 
 
 @app.route('/towards_homepage', methods= ['POST', 'GET'])
 def towards_homepage():
+
+    """ Renders the homepage.html file under templates (Not in use in current version of project)"""
     return render_template('homepage.html')
 
 @app.route('/registration_form')
 def registration_form():
+
+    """This route renders registration_form"""
     return render_template('registration_form.html')
 
 global var
 var = 0
 @app.route('/register_new_user', methods = ['POST', 'GET'])
 def register_new_user():
-    #code for redirecting user to the login page after the user succesfully registers
+
+    """Route for handling registration of a user
+    The user details are simultaeneously pushed into two databses i.e. User db and Role db"""
     global var  
-    print("first one")
+    
     if request.method == 'POST':
-        print("success underway")
+        
         name = request.form['name']
         username = request.form['username']
         password = request.form['password']
         access = request.form['access']
         if password =='' or username == '' or name =='' :
             return render_template('registration_form.html', info = 'Please fill all the fields')
-        print("print this")
-        print(type(username))
-        print(type(access))
-        #here we put the new user data into database
+        
+        # here we put the new user data into database
         p = User(name = name, username = username, password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(14)))
         db.session.add(p)
-        print(var)
         var = Role.query.count()
         q = Role(id = var, username = username, role = access)
         
@@ -104,35 +107,25 @@ def register_new_user():
         return render_template('registration_form.html',info = 'You have sucessfully registered!')
     else :
         return "new user registration was failed"
-#returns encoded text given any plaintext file
+
 def encrypt_plaintext(text):
 
+    """Encrypts any plaintext value into a corresponding base64 value"""
     modified_plaintext = text.encode('utf-8')
     encoded_text = base64.b64encode(modified_plaintext)	
     return encoded_text
-#returns the decrypted ciphertext
+
 def decrypt_plaintext(text):
 
+    """Decrypts any base64 encoded text into a corresponding plaintext """
     sample_string_bytes = base64.b64decode(text)
     sample_string = sample_string_bytes.decode("ascii")
     return sample_string	
 
-
-@app.route('/admin_login')
-def admin_login():
-    return render_template('admin_login.html')
-    #return "this is the admin login page and here redirection to the login page for admin shall happen"
-
-@app.route('/client_login')
-def client_login():
-    return render_template('client_login.html')
-    #return "this is the client login page and here redirection to the client login page shall happen"
-
 @app.route('/delete/<string:id>')
 def erase(id):
      
-    # deletes the data on the basis of unique id and
-    # directs to home page
+    """Deletion of a specific cipher entity from database through unique ID"""
     data = ciphers.query.get(id)
     db.session.delete(data)
     db.session.commit()
@@ -141,6 +134,8 @@ def erase(id):
 
 @app.route('/delete_user/<string:id>')
 def erase_user(id):
+
+    """Deletion of a specific User database entity through the unique ID"""
     data = User.query.get(id)
     db.session.delete(data)
     db.session.commit()
@@ -148,78 +143,85 @@ def erase_user(id):
 
 @app.route('/delete_role/<string:id>')
 def erase_role(id):
+
+    """Deletion of a specific entity from Role database through the unique ID"""
     data = Role.query.get(id)
     db.session.delete(data)
     db.session.commit()
     return redirect('/displayall_roles')
 
-@app.route('/success')
-def success():
-    return render_template('homepage.html')
-    
 
-# DATABASE RENDERING ROUTES
 @app.route('/displayall',methods = ['POST','GET'])
 def displayall():
-    cipher = ciphers.query.all()
-    return render_template('index.html', cipher = cipher)
 
+    """Querying all the entries of Cipher Database at once and presenting them onto a html template"""
+    new_log = logs.query.all()
+    return render_template('index.html', log = new_log)
+
+@app.route('/displayall_log', methods = ['POST', 'GET'])
+def displayall_log():
+
+    """ Displays the Database containing Every entry made by client, saved as a log on server side """
+    log = Logs.query.all()
+    return render_template('index_log.html', log = log)
 
 @app.route('/displayall_user',methods = ['POST','GET'])
 def displayall_user():
+
+    """Querying all the entries of Cipher Database at once and presenting them onto a html template"""
     user = User.query.all()
     return render_template('index_user.html', user = user )
     
 @app.route('/displayall_roles', methods = ['POST','GET'])
 def displayall_roles():
+
+    """Querying all the entries of Cipher Database at once and presenting them onto a html template"""
     role = Role.query.all()
     return render_template('index_role.html', role = role )
 
 @app.route('/to_client_page')
 def to_client_page():
+
+    """Redirects the user to client page after login"""
     return render_template('client_page.html', info = 'You are logged in!')
 
-@app.route('/to_manager_page')
+@app.route('/to_manager_page')   
 def to_manager_page():
+
+    """Redirects the user to client page after login"""
     return render_template('manager_page.html', info = 'You are logged in!')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+
+    """Handles the user login in the system after one enters there credentials
+       Types of credential checks taking place :
+       1. whether user has left the password or username field empty
+       2. whether the user has entered incorrect username or password
+       3. if the corresponding password for a username entered is correct (according to database)
+
+       At the time of login another check is made w.r.t. the role of user"""
+       
     if request.method == 'POST':
         username = request.form['username']
         raw_username = non_encrypted_username = username
 
         password = request.form['password']     
         raw_password = password
-        
-        
-        #print(query) 
-        #check if password matches or not and also check that if role based on username 
-        #if the username is related to manager then redirect to manager_page and if client then redirect to manager_page
-
-        
-        
-        
-
-        print(db.session.query(User.id).filter_by(username = username))
+      
         db.session.query(User.id).filter_by()
-        matching_roles = db.session.query(Role.id).filter_by(username = non_encrypted_username)
         matching_users = db.session.query(Role.role).filter_by(username = non_encrypted_username)
         matching_password = User.query.filter_by(username = username)
         val = ""
-        print("imp")
-        print(username)
+        session['login'] = raw_username
         pass_to_check = ""
         for user in matching_users :
             val = user.role
-        print(val)
-        cnt = 0
+      
         pass_to_check = ""
         for value in matching_password:
             pass_to_check  = value.password
-            print(cnt)
-        print(password)
-        print(pass_to_check)
+           
         if raw_username == '' or raw_password == '':
             return render_template('form.html', info = 'Username and Password cannot be empty')
 
@@ -234,149 +236,69 @@ def login():
 
     else:
         return render_template('form.html')
-        """Version= request.args.get('Version')
-        SystemSerialNumber = request.args.get('SystemSerialNumber')
-        Licenses = request.args.get('Licenses')
-        return redirect(url_for('success',Version=Version,SystemSerialNumber=SystemSerialNumber,Licenses=Licenses))"""
+       
 
 
-
-@app.route('/encipher/<Version> /n <SystemNumber> /n <License>/n')
-def encipher(Version, SystemNumber, License):
-    plaintext = "NonStop ESS License \nVersion : 1 \n SystemNumber : "+SystemNumber+"\nLicense:1"
-    print("here i am printing" + plaintext)
-    modified_plaintext = plaintext.encode('utf-8')
-    encoded_text = base64.b64encode(modified_plaintext)	
-    with open('./saved_files/base64_encoded_file','wb') as binary_file:
-        binary_file.write(encoded_text)
-    
-   
-    if encoded_text != '':
-        p = ciphers(cipher = encoded_text)
-        db.session.add(p)
-        db.session.commit()
-        return redirect(url_for("to_client_page")+ "#myid")
-    else :
-        return redirect('/')
-    return encoded_text 
-
-@app.route('/download', methods = ['POST','GET'])
+@app.route('/download', methods = ['GET','POST'])
 def download_file():
-    print("into this function")
+
+    """A file is created with base64 encoded value of plaintext (see below in this function).
+    The webpage prompts User to provide a desired filename and thereafter the encrypted file
+    gets saved onto the client desktop """
+
     if request.method == 'POST':
-        print("in this request")
+    
         SystemNumber =  request.form['SystemNumber']
-        License = request.form.get('license')
+        License = 'license' in request.form
+    
         filename = request.form['filename']
-        if SystemNumber == '':
+        if SystemNumber == '': 
             return render_template('client_page.html', info = 'SystemNumber cannot be empty!')
 
         if filename == '':
             return render_template('client_page.html', info = 'Filename cannot be empty!')
-        print("the below value is of license")
-        print(type(License))
-        if License == "on":
+       
+
+        if License == True:
             License_pass_value = '1'
         else : 
             License_pass_value = '0'
         
-        ciphertext ={
-            'Version'  : 1, 
-            'SystemNumber' : SystemNumber,
-            'License' : 1
-        }
+        plaintext = "NonStop ESS License \nVersion : 1 \n SystemNumber : "+SystemNumber+"\nLicense :"+License_pass_value
 
-        plaintext = "NonStop ESS License \nVersion : 1 \n SystemNumber : "+SystemNumber+"\nLicense:"+License_pass_value;
-        print("here i am printing" + plaintext)
         modified_plaintext = plaintext.encode('utf-8')
+        
         encoded_text = base64.b64encode(modified_plaintext)	
+        entry = Logs(username = session['login'], cipher = encoded_text,date = datetime.datetime.now())
+        db.session.add(entry)
+        db.session.commit()
         with open(filename,'wb') as binary_file:
-            binary_file.write(encoded_text)
+            binary_file.write(encoded_text) 
 
         p = "base64_encoded_file.txt"
         return send_file(filename,as_attachment = True)
     else:
-        return "hello gys"
-
-@app.route('/encode', methods = ['POST','GET'])
-def encode():
-    print("this is encode function")
-    if request.method == 'POST':
-
-        SystemNumber =  request.form['SystemNumber']    
-        License = request.form.get('License')
-        print("the below value is of license")
-        print(type(License))
-        if License == "on":
-            License_pass_value = 1
-        else : 
-            License_pass_value = 0
-        
-        ciphertext ={
-            'Version'  : 1, 
-            'SystemNumber' : SystemNumber,
-            'License' : 1
-        }
-
-        
-        return redirect(url_for('encipher',Version = 1, SystemNumber =  SystemNumber, License = License_pass_value))
-    return render_template('login.html') 
-
-    
-
-@app.route('/decipher/<cipher>')
-def decipher(cipher):
-    #converts the cipher into plaintext
-    #base64_string =" R2Vla3NGb3JHZWVrcyBpcyB0aGUgYmVzdA =="
-    base64_bytes = cipher.encode("ascii")
-    
-    sample_string_bytes = base64.b64decode(base64_bytes)
-    sample_string = sample_string_bytes.decode("ascii")
-    print("printing sample string " + sample_string)
-    return sample_string	
-    
-# take input from user and first encipher the input and then save the encrypted part into the DB.
-# when the user 
+        return redirect('/')
 
 
 @app.route('/decode_file', methods = ['POST','GET'])
 def decode_file():
+
+    """ Takes input in form of a base64 encoded file and then decrypts that file"""
     if request.method == 'POST':
         text_to_be_encoded = request.form['Choose File']
-        file1 = open(text_to_be_encoded, "r+")
-        print(file1.read())
-        
+        file1 = open(text_to_be_encoded, "r+")  
         
 
         with open(text_to_be_encoded,'r') as file:
             to_print = file.read()
         base64_bytes = to_print.encode("ascii")
-        print("this is to_print" + to_print)
+        
     
         sample_string_bytes = base64.b64decode(base64_bytes)
         sample_string = sample_string_bytes.decode("ascii")
         return render_template('manager_page.html', info = sample_string)
-        #print(text_to_be_encoded + "she")
     return redirect('/decode_file')
-
-
-
-@app.route('/decode', methods = ['POST', 'GET'])
-def decode():
-    #return "successfully executed decode function"
-    if request.method == 'POST':
-        cipher = request.form['Choose File']
-        cipher += "=" * ((4 - len(cipher) % 4) % 4)
-        print(cipher,base64.b64encode(base64.b64decode(cipher)),"she")
-        c = 0
-        if base64.b64encode(base64.b64decode(cipher)) == bytes(cipher, 'utf-8') :
-            return redirect(url_for('decipher', cipher = cipher))
-        
-        else :
-            return "The string you entered is not base64 encoded"
-    else:
-        return "hello_world"
-
 
 
 if __name__ == '__main__':
